deec5143fb2b106ebf384d62592acce11303bd0b
Eino <eino.ahrio@tuni.fi>
2020-06-08 15:51:34 +0300

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,13 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
+#<<<<<<< HEAD
+#<<<<<<< HEAD
 #<<<<<<< HEAD
 #=======
 print("Hello once again")
 #>>>>>>> tuni392/feature/add-logging
-=======
+#=======
 print("Hello darkness my old friend")
->>>>>>> 380b3e6c7b69204e182e6ab09da42426fb333590
-=======
+#>>>>>>> 380b3e6c7b69204e182e6ab09da42426fb333590
+#=======
 print("I've come to talk with you again")
->>>>>>> tuni393/feature/modify-print
+#>>>>>>> tuni393/feature/modify-print
